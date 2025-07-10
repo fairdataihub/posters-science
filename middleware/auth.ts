b26409ab46749480d49041b387fdc9e70b7571a5
Sanjay Soundarajan <sanjay.16@live.com>
2025-07-09 18:24:43 -0700

    ci: disable middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,9 +1,8 @@
 export default defineNuxtRouteMiddleware((to, _from) => {
-  const { loggedIn } = useUserSession();
-
-  if (!loggedIn.value) {
-    return navigateTo(
-      `/login${to ? "?redirect=" + encodeURIComponent(to.path) : ""}`,
-    );
-  }
+  // const { loggedIn } = useUserSession();
+  // if (!loggedIn.value) {
+  //   return navigateTo(
+  //     `/login${to ? "?redirect=" + encodeURIComponent(to.path) : ""}`,
+  //   );
+  // }
 });
