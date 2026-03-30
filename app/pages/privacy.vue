4d4e044320e6f1dc5a6f6976a84d538661b875c0
Sanjay Soundarajan <ssoundarajan@calmi2.org>
2026-03-30 16:25:50 -0700

    feat: add nuxt/content privacy and terms pages

---
+++ app/pages/privacy.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "Privacy Policy - Posters.Science",
+  description:
+    "How the Posters.Science platform collects, uses, and protects personal information related to the use of the platform.",
+});
+
+const { data: privacy } = await useAsyncData(() =>
+  queryCollection("content").path("/privacy").first(),
+);
+</script>
+
+<template>
+  <UContainer class="py-12">
+    <ContentRenderer v-if="privacy" :value="privacy" />
+
+    <div v-else>Document not found</div>
+  </UContainer>
+</template>
---
+++ app/pages/terms.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "Terms of Use - Posters.Science",
+  description:
+    "Terms and conditions governing the use of the Posters.Science platform and associated services.",
+});
+
+const { data: terms } = await useAsyncData(() =>
+  queryCollection("content").path("/terms").first(),
+);
+</script>
+
+<template>
+  <UContainer class="py-12">
+    <ContentRenderer v-if="terms" :value="terms" />
+
+    <div v-else>Document not found</div>
+  </UContainer>
+</template>
